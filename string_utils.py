bcc6cef79e1a0f8e587830f0c1a3f64f76ab81c6
Yuxuan Liu <uken.ryu.2@tier4.jp>
2024-05-07 10:04:31 +0900

    WIP: improve development documentation and formatting
    Signed-off-by: Yuxuan Liu <uken.ryu.2@tier4.jp>

--- string_utils.py
+++ string_utils.py
@@ -3,53 +3,103 @@ import re
 import os
 
 patterns = {
-    'var': r'\$\((var) ([^\)]+)\)',
-    'env': r'\$\((env) ([^\s]+)(?:\s+([^\)]+))?\)', 
-    'eval': '\$\((eval) ([^\)]+)\)',
-    'find-pkg-share': r'\$\((find-pkg-share) ([^\)]+)\)',
+    "var": r"\$\((var) ([^\)]+)\)",
+    "env": r"\$\((env) ([^\s]+)(?:\s+([^\)]+))?\)",
+    "eval": "\$\((eval) ([^\)]+)\)",
+    "find-pkg-share": r"\$\((find-pkg-share) ([^\)]+)\)",
 }
 
+
 def clean_eval_variables(string):
+    """Remove quotes and spaces from a string, to obtain the 'value' of a variable."""
     return string.replace('"', "").replace("'", "").strip()
 
-def analyze_string(input_string, context, local_context, base_namespace):
-    """Resolve substitutions recursively in a given string."""
-    
+
+def analyze_string(
+    input_string: str, context: dict, local_context: dict, base_namespace: str
+):
+    """Resolve substitutions recursively in a given string.
+
+    Args:
+    context: The arugments and variables context of the current XML file, which is defined by the arg tag and will be passed to the included file
+    local_context: The local variable context of the current XML file, which is defined by the let tag
+    base_namespace: The current namespace of the XML file
+
+    Returns:
+    The string with all substitutions resolved.
+    """
+
     def replace_match(match):
         # Determine type and execute corresponding logic
-        if match.group(1) == 'var':
-            variable_name = analyze_string(match.group(2), context, local_context, base_namespace)
+        if match.group(1) == "var":
+            variable_name = analyze_string(
+                match.group(2), context, local_context, base_namespace
+            )  # Recursively resolve inner substitutions
+            ## Check if the variable is in the local context
             var_value = local_context.get(variable_name, None)
             if var_value is None:
+                ## Check if the variable is in the global context
                 var_value = context.get(os.path.join(base_namespace, variable_name))
             return var_value
-        elif match.group(1) == 'env':
-            var_name = analyze_string(match.group(2), context, local_context, base_namespace)
+        elif match.group(1) == "env":
+            var_name = analyze_string(
+                match.group(2), context, local_context, base_namespace
+            )  # Recursively resolve inner substitutions
             default_value = analyze_string(
-                match.group(3) if match.group(3) is not None else '', context, local_context, base_namespace)
+                match.group(3) if match.group(3) is not None else "",
+                context,
+                local_context,
+                base_namespace,
+            )
             return os.getenv(var_name, default_value)
-        elif match.group(1) == 'eval':
-            expression = analyze_string(match.group(2), context, local_context, base_namespace)
-            # deal with "=="
+        elif match.group(1) == "eval":
+            expression = analyze_string(
+                match.group(2), context, local_context, base_namespace
+            )
+            ## Deal with different types of expressions, currently only support ones actually used in the launch files
+            ## TODO: Refactor and suport more types of expressions
             variables = expression.split("==")
             if len(variables) == 2:
-                return str(clean_eval_variables(variables[0]) == clean_eval_variables(variables[1]) )
+                return str(
+                    clean_eval_variables(variables[0])
+                    == clean_eval_variables(variables[1])
+                )
             variables = expression.split(">=")
             if len(variables) == 2:
-                return str(clean_eval_variables(variables[0]) >= clean_eval_variables(variables[1]) )
+                return str(
+                    clean_eval_variables(variables[0])
+                    >= clean_eval_variables(variables[1])
+                )
+            variables = expression.split("<")
+            if len(variables) == 2:
+                return str(
+                    clean_eval_variables(variables[0])
+                    < clean_eval_variables(variables[1])
+                )
             variables = expression.split("+")
             if len(variables) == 2:
-                return str(clean_eval_variables(variables[0]) + clean_eval_variables(variables[1]))
+                return str(
+                    clean_eval_variables(variables[0])
+                    + clean_eval_variables(variables[1])
+                )
 
-        elif match.group(1) == 'find-pkg-share':
-            package_name = analyze_string(match.group(2), context, local_context, base_namespace)  # Recursively resolve inner substitutions
+        elif match.group(1) == "find-pkg-share":
+            package_name = analyze_string(
+                match.group(2), context, local_context, base_namespace
+            )  # Recursively resolve inner substitutions
             package_dir = get_package_share_directory(package_name)
             return package_dir
-        
-        return ''
+
+        return ""
 
     # Loop to ensure all substitutions are resolved
     for key, pattern in patterns.items():
+        """
+        1. Solve all variables.
+        2. Solve all environment variables.
+        3. Solve all eval expressions.
+        4. Solve all find-pkg-share expressions.
+        """
         while True:
             old_string = input_string
             input_string = re.sub(pattern, replace_match, input_string)
--- xml_parsing.py
+++ xml_parsing.py
@@ -4,117 +4,188 @@ import os
 from string_utils import analyze_string, find_linked_path
 from tree_utils import LaunchTree
 from launch_node_utils import parse_node_tag
+from typing import List, Optional
 
-def check_if_run(tag, base_name, context, local_context):
-    if tag.get('if'):
-        if_value = analyze_string(tag.get('if'), context, local_context, base_name)
-        if_value = if_value.lower() == 'true'
+
+def check_if_run(tag: ET.Element, base_name: dict, context: dict, local_context: dict):
+    """Many tag has a if and unless attribute, this function checks if the tag should be run or not."""
+    if tag.get("if"):
+        if_value = analyze_string(tag.get("if"), context, local_context, base_name)
+        if_value = if_value.lower() == "true"
         if not if_value:
             return False
-    if tag.get('unless'):
-        unless_value = analyze_string(tag.get('unless'), context, local_context, base_name)
-        unless_value = unless_value.lower() == 'true'
+    if tag.get("unless"):
+        unless_value = analyze_string(
+            tag.get("unless"), context, local_context, base_name
+        )
+        unless_value = unless_value.lower() == "true"
         if unless_value:
             return False
     return True
-    
-def process_include_tag(include_tag: ET.Element, context, local_context, base_namespace, group_base_namespace=None):
+
+
+def process_include_tag(
+    include_tag: ET.Element,
+    context: dict,
+    local_context: dict,
+    base_namespace: str,
+    group_base_namespace: Optional[str] = None,
+):
+    """Process the include tag, which includes another XML file.
+
+    include_tag: The include XML node tag to process
+    context: The arugments and variables context of the current XML file, which is defined by the arg tag and will be passed to the included file
+    local_context: The local variable context of the current XML file, which is defined by the let tag
+    base_namespace: The current namespace of the XML file
+    group_base_namespace: The namespace of the current group tag (affect the namespace of the included file)
+    """
     if group_base_namespace is None:
         group_base_namespace = base_namespace
-    included_file = include_tag.get('file')
-    included_file = analyze_string(included_file, context, local_context, base_namespace)
+    included_file = include_tag.get("file")
+    included_file = analyze_string(
+        included_file, context, local_context, base_namespace
+    )
     included_file = find_linked_path(included_file)
+
+    argument_dict = dict()
     # temp_context = dict()
     for child in include_tag:
-        if child.tag == 'arg':
-            value = analyze_string(child.get('value'), context, local_context, base_namespace)
-            name = analyze_string(os.path.join(group_base_namespace, child.get('name')), context, local_context, base_namespace)
-            if name == 'config_file':
-                print("we get config_file", child.get('value'))
+        if child.tag == "arg":
+            value = analyze_string(
+                child.get("value"), context, local_context, base_namespace
+            )
+            name = analyze_string(
+                os.path.join(group_base_namespace, child.get("name")),
+                context,
+                local_context,
+                base_namespace,
+            )
+            if name == "config_file":
+                print("we get config_file", child.get("value"))
             print(name, value)
-            context[name] = value
+            argument_dict[name] = value
+    temp_context = deepcopy(context)
+    temp_context.update(argument_dict)
     if included_file:
         print(f"Reading included file: {included_file}")
-        if included_file.endswith('.launch.xml'):
-            return parse_xml(included_file, group_base_namespace, context)
+        if included_file.endswith(".launch.xml"):
+            print(temp_context)
+            return parse_xml(included_file, group_base_namespace, temp_context)
     return context
 
-def parse_argument_tag(argument_tag, base_namespace, context, local_context):
-    argument_name = os.path.join(base_namespace, argument_tag.get('name'))
-    if argument_tag.get('default'):
+
+def parse_argument_tag(
+    argument_tag: ET.Element, base_namespace: str, context: dict, local_context: dict
+):
+    argument_name = os.path.join(base_namespace, argument_tag.get("name"))
+    if argument_tag.get("default"):
         if argument_name not in context:
-            if argument_name == 'launch_driver':
-                print("-------------------------we get launch_driver", argument_tag.get('default'))
-            context[argument_name] = analyze_string(argument_tag.get('default'), context, local_context, base_namespace)
+            if argument_name == "launch_driver":
+                print(
+                    "-------------------------we get launch_driver",
+                    argument_tag.get("default"),
+                )
+            context[argument_name] = analyze_string(
+                argument_tag.get("default"), context, local_context, base_namespace
+            )
     return context
 
-def parse_let_tag(let_tag, base_namespace, context, local_context):
-    argument_name = let_tag.get('name')
-    if let_tag.get('value'):
-        local_context[argument_name] = analyze_string(let_tag.get('value'), context, local_context, base_namespace)
+
+def parse_let_tag(
+    let_tag: ET.Element, base_namespace: str, context: dict, local_context: dict
+):
+    argument_name = let_tag.get("name")
+    if let_tag.get("value"):
+        local_context[argument_name] = analyze_string(
+            let_tag.get("value"), context, local_context, base_namespace
+        )
     return context
 
-def parse_group_tag(group_tag, base_namespace:str, context, local_context, parent_file_space=None):
+
+def parse_group_tag(
+    group_tag: ET.Element,
+    base_namespace: str,
+    context: dict,
+    local_context: dict,
+    parent_file_space: Optional[str] = None,
+):
     if parent_file_space is None:
         parent_file_space = base_namespace
     # find the push-ros-namespace tag inside the children
     group_base_namespace = deepcopy(base_namespace)
     for child in group_tag:
-        if child.tag == 'push-ros-namespace':
-            if child.get('namespace').strip() == '/':
+        if child.tag == "push-ros-namespace":
+            if child.get("namespace").strip() == "/":
                 continue
-            group_base_namespace = f"{base_namespace}/{child.get('namespace').strip('/')}"
+            group_base_namespace = (
+                f"{base_namespace}/{child.get('namespace').strip('/')}"
+            )
             print(f"Setting ROS namespace to {group_base_namespace} inside group")
 
     ## find all other children
     for child in group_tag:
-        process_tag(child, base_namespace, context, local_context, group_base_namespace=group_base_namespace)
+        process_tag(
+            child,
+            base_namespace,
+            context,
+            local_context,
+            group_base_namespace=group_base_namespace,
+        )
 
     if group_base_namespace != base_namespace:
         print(f"Exiting group with namespace {group_base_namespace}")
     return context
 
 
-def process_tag(tag, base_namespace, context, local_context, group_base_namespace=None):
+def process_tag(
+    tag: ET.Element,
+    base_namespace: str,
+    context: dict,
+    local_context: dict,
+    group_base_namespace: Optional[str] = None,
+):
     if group_base_namespace is None:
         group_base_namespace = base_namespace
     if not check_if_run(tag, base_namespace, context, local_context):
         return context
 
-    if tag.tag == 'arg':
+    if tag.tag == "arg":
         context = parse_argument_tag(tag, base_namespace, context, local_context)
-    elif tag.tag == 'let':
+    elif tag.tag == "let":
         context = parse_let_tag(tag, base_namespace, context, local_context)
-    elif tag.tag == 'group':
+    elif tag.tag == "group":
         context = parse_group_tag(tag, base_namespace, context, local_context)
-    elif tag.tag == 'include':
-        context = process_include_tag(tag, context, local_context, base_namespace, group_base_namespace)
-    elif tag.tag == 'node':
+    elif tag.tag == "include":
+        context = process_include_tag(
+            tag, context, local_context, base_namespace, group_base_namespace
+        )
+    elif tag.tag == "node":
         context = parse_node_tag(tag, base_namespace, context, local_context)
     return context
 
 
-def parse_xml(file_path, namespace="", context={}):
-    """Recursively parse XML files, handling <include> tags."""
+def parse_xml(file_path: str, namespace: str = "", context: dict = {}):
+    """Recursively parse XML files, handling <include> tags. For each file, the namespace should be the same"""
     full_path = os.path.join(file_path)
     tree = ET.parse(full_path)
     root = tree.getroot()
-    
+
     # Process each node in the XML
     local_context = dict()
     for tag in root:
         process_tag(tag, namespace, context, local_context)
     return context
 
+
 if __name__ == "__main__":
     # Start parsing from the main XML file
-    main_file = '/home/ukenryu/autoware/src/launcher/autoware_launch/autoware_launch/launch/logging_simulator.launch.xml'  # Update this path to your main XML file
+    main_file = "/home/ukenryu/autoware/src/launcher/autoware_launch/autoware_launch/launch/logging_simulator.launch.xml"  # Update this path to your main XML file
     context_tree = LaunchTree()
     context = dict()
-    context['__tree__'] = context_tree
+    context["__tree__"] = context_tree
 
     ## arguments
-    context['vehicle_model'] = 'sample_vehicle'
-    context['sensor_model'] = 'sample_sensor_kit'
+    context["vehicle_model"] = "sample_vehicle"
+    context["sensor_model"] = "sample_sensor_kit"
     context = parse_xml(main_file, context=context)
     print(context)
